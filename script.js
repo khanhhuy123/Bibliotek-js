cae6147ea6049e6ad0628fa41eae573f256a4489
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-02-09 21:24:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-const Library = [];
+let library = [];
 
-const Book = {
-    tittle: "Null",
-    author: "Null",
-    isbn: Int,
-
-    addBook: function(title, author, isbn){
-        this.tittle = title;
-        this.author = author;
-        this.isbn = isbn;
-        Library.push(Book);
-    },
+const book1 = {
+    title: "John sword",
+    author: "JK1",
+    isbn: 123123,
+    isLoan: false,
 };
+const book2 = {
+    title: "John's adventure",
+    author: "JK2",
+    isbn: 123124,
+    isLoan: false
+};
+const book3 = {
+    title: "John in jungle",
+    author: "JK3",
+    isbn: 123125,
+    isLoan: false
+};
+const book4 = {
+    title: "John John",
+    author: "JK4",
+    isbn: 123126,
+    isLoan: false
+};
+
+library.push(book1)
+library.push(book2)
+library.push(book3)
+library.push(book4)
+
+function loanBook(bookIsbn){
+    for (let i = 0; i < library.length; i++){
+        if(bookIsbn == library[i].isbn){
+            library[i].isLoan = true
+        }
+    }
+};
+
+
 
